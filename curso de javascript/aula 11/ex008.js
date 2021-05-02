6783cae7a8647f5a9008838042372b8ae2e75436
Macedo3 <82056067+Macedo3@users.noreply.github.com>
2021-05-02 20:48:33 -0300

    Add files via upload

---
+++ curso de javascript/aula 11/ex008.js
@@ -0,0 +1,8 @@
+var rel = 78.2
+console.log(`A velocidade do seu carro é ${rel}Km/h`)
+
+if(rel >60) { //Condição simples!
+    console.log('Você ultrapassou a velocidade permitida. Foi multado.')
+}
+
+console.log('Dirija sempre com segurança!')
