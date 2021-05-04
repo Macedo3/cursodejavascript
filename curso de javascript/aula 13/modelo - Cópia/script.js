0d0212eb11178b2b6f51284c869f8e868b0bdd54
Macedo3 <82056067+Macedo3@users.noreply.github.com>
2021-05-04 08:14:42 -0300

    Add files via upload

--- curso de javascript/aula 13/modelo - Cópia/script.js
+++ curso de javascript/aula 13/modelo - Cópia/script.js
@@ -1,22 +1,22 @@
 function carregar() {
     var msg = window.document.getElementById('msg')
-    var img = window.document.getElementById('img')
+    var img = window.document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
     msg.innerHTML = `Agora são ${hora} horas`
-}
 
-if(hora >= 0 || hora < 12) {
-    //BOM DIA
-    img.src = 'fotomanha.png'
-}
+    if(hora > 4 && hora < 12) {
+        //BOM DIA
+        img.src = 'fotomanha.png'
+    }
 
-else if(hora >= 12 || hora < 23){
-    //BOA TARDE
-    img.src = 'fototarde.png'
-}
+    else if(hora >= 12 && hora < 18){
+        //BOA TARDE
+        img.src = 'fototarde.png'
+    }
 
-else {
-    //BOA NOITE
-    img.src = 'fotonoite.png'
+    else {
+        //BOA NOITE
+        img.src = 'fotonoite.png'
+    }
 }
